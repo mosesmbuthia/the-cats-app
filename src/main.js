248f2ac3c22bbc0b96f978721414b0773312e13f
mosesmbuthia <mbuthiamoses435@gmail.com>
2025-03-02 08:47:37 +0300

    added JavaScript function

---
+++ src/main.js
@@ -0,0 +1,8 @@
+const form = document.getElementById("form");
+form.addEventListener("submit" , async (e) => {
+    e.preventDefault();
+    const formVal = document.getElementById("form");
+    const factVal = document.getElementById("factInput");
+    const displaVal = document.getElementById("display");
+    console.log(displaVal);
+});
